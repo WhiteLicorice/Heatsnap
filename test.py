40345a097926706e997426550612bcc5327da4a5
RB <rbjocsing@up.edu.ph>
2024-05-18 19:58:02 +0800

    Heatsnap environment setup

---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+
+print("Validating PyTorch installation...")
+import torch
+print(torch.__version__)
+torch.cuda.is_available()
+
+print("Validating Keras installation...")
+#   Declare our Keras backend
+os.environ["KERAS_BACKEND"] = "torch"
+import keras
+print(keras.__version__)
+
+print("Validating TensorFlow installation...")
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'   #   Avoid floating-point round-off errors from different computation orders
+import tensorflow as tf
+print(tf.__version__)
+
+print("Validating KerasCV installation...")
+import keras_cv
+print(keras_cv.__version__)
+
+print("Validating Pandas installation...")
+import pandas as pd
+print(pd.__version__)
+
+print("All KerasPyCV dependencies installed!")
